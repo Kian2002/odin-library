33f8bdd63aa45d99b200b2afe36fb9ddbea3884c
Kian2002 <canadakurdistan@gmail.com>
2023-01-23 21:28:09 -0800

    display books to user

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
 
-  this.info = () => {
+  this.getInfo = () => {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${
       this.read ? "read" : "not yet read"
     }`;
   };
 }
 
-const hobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+const addBookToLibrary = (...books) => {
+  books.forEach((book) => {
+    myLibrary.push(book);
+  });
+};
 
-console.log(hobbit.info());
+const hobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+const testBook = new Book("test", "testman", 100, false);
+
+addBookToLibrary(hobbit, testBook);
+
+const displayBooks = () => {
+  myLibrary.forEach((book) => {
+    console.log(book);
+    const title = document.createElement("div");
+    title.innerText = book.getInfo();
+    document.getElementById("container").appendChild(title);
+  });
+};
+
+displayBooks();
